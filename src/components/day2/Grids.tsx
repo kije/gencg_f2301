08276e523802319c37e628369bb100ccec222903
Kim D. Jeker <git@kije.ch>
2023-09-05 13:36:47 +0200

    day2 update

--- src/components/day2/Grids.tsx
+++ src/components/day2/Grids.tsx
@@ -46,7 +46,14 @@ export const Grid1: FC<GridProps> = ({
         });
       });
     },
-    [canvasWidth, canvasHeight, numTilesX, numTilesY, margin],
+    [
+      canvasWidth,
+      canvasHeight,
+      numTilesX,
+      numTilesY,
+      margin,
+      tileSizeWithMargin,
+    ],
   );
 
   return <ResponsiveSketch setup={setup} />;
@@ -85,18 +92,83 @@ export const Grid2: FC<GridProps> = ({
           p5.resetMatrix();
           // remember: composing transformations is done by multiplying the matrices, thus operations need to be in reverse order
           p5.translate(x0, y0);
+          p5.translate(tileSize / 2, tileSize / 2);
+          p5.rotate(angle);
+          p5.translate(tileSize / -2, tileSize / -2);
+
+          p5.rect(0, 0, tileSize, tileSize);
+        });
+      });
+    },
+    [
+      canvasWidth,
+      canvasHeight,
+      numTilesX,
+      numTilesY,
+      margin,
+      tileSize,
+      tileSizeWithMargin,
+    ],
+  );
+
+  return <ResponsiveSketch setup={setup} />;
+};
+
+export const Grid3Interesting: FC<GridProps> = ({
+  numTilesX,
+  numTilesY,
+  margin = 2,
+  contentWidth = PAGE_CONTENT_WIDTH,
+}) => {
+  const tileSize = contentWidth / numTilesX - 2 * margin;
+  const tileSizeWithMargin = tileSize + 2 * margin;
+
+  const canvasWidth = tileSizeWithMargin * numTilesX;
+  const canvasHeight = tileSizeWithMargin * numTilesY;
+
+  const step = useRef(0);
+
+  const setup = useCallback<SketchProps["setup"]>(
+    (p5, canvasParentRef) => {
+      p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
+    },
+    [canvasWidth, canvasHeight],
+  );
+
+  const draw = useCallback<NonNullable<SketchProps["draw"]>>(
+    (p5) => {
+      p5.clear();
+      p5.stroke(255, 255, 255);
+      p5.strokeWeight(1);
+      p5.noFill();
+
+      forEach(range(0, numTilesX), (column) => {
+        forEach(range(0, numTilesY), (row) => {
+          const x0 = margin + column * tileSizeWithMargin;
+          const y0 = margin + row * tileSizeWithMargin;
+
+          let angle = (column / numTilesX) * (row / numTilesY) * Math.PI * 1.1;
+          let angle2 = p5.map(step.current % 60, 0, 60, 0, p5.TWO_PI);
+
+          // reset rotation & translation
+          p5.resetMatrix();
+          // remember: composing transformations is done by multiplying the matrices, thus operations need to be in reverse order
+          p5.translate(x0, y0);
           p5.translate(tileSizeWithMargin / 2, tileSizeWithMargin / 2);
+          p5.rotate(angle2);
           p5.rotate(angle);
           p5.translate(tileSizeWithMargin / -2, tileSizeWithMargin / -2);
 
           p5.rect(0, 0, tileSize, tileSize);
         });
       });
+
+      step.current++;
     },
-    [canvasWidth, canvasHeight, numTilesX, numTilesY, margin],
+    [numTilesX, numTilesY, margin, tileSize, tileSizeWithMargin],
   );
 
-  return <ResponsiveSketch setup={setup} />;
+  return <ResponsiveSketch setup={setup} draw={draw} />;
 };
 
 export const Grid3: FC<GridProps> = ({
@@ -139,10 +211,10 @@ export const Grid3: FC<GridProps> = ({
           p5.resetMatrix();
           // remember: composing transformations is done by multiplying the matrices, thus operations need to be in reverse order
           p5.translate(x0, y0);
-          p5.translate(tileSizeWithMargin / 2, tileSizeWithMargin / 2);
+          p5.translate(tileSize / 2, tileSize / 2);
           p5.rotate(angle2);
           p5.rotate(angle);
-          p5.translate(tileSizeWithMargin / -2, tileSizeWithMargin / -2);
+          p5.translate(tileSize / -2, tileSize / -2);
 
           p5.rect(0, 0, tileSize, tileSize);
         });
@@ -150,7 +222,7 @@ export const Grid3: FC<GridProps> = ({
 
       step.current++;
     },
-    [numTilesX, numTilesY, margin],
+    [numTilesX, numTilesY, margin, tileSize, tileSizeWithMargin],
   );
 
   return <ResponsiveSketch setup={setup} draw={draw} />;
@@ -191,9 +263,10 @@ export const Grid4: FC<GridProps> = ({
 
           let angle = (column / numTilesX) * (row / numTilesY) * Math.PI;
           let angle2 = p5.map(
-            step.current % (((column + 1) / numTilesX) * 350),
+            step.current %
+              ((column + 1 / numTilesX) * (row + 1 / numTilesY) * 350),
             0,
-            ((column + 1) / numTilesX) * 350,
+            (column + 1 / numTilesX) * (row + 1 / numTilesY) * 350,
             0,
             p5.TWO_PI,
           );
@@ -202,10 +275,10 @@ export const Grid4: FC<GridProps> = ({
           p5.resetMatrix();
           // remember: composing transformations is done by multiplying the matrices, thus operations need to be in reverse order
           p5.translate(x0, y0);
-          p5.translate(tileSizeWithMargin / 2, tileSizeWithMargin / 2);
+          p5.translate(tileSize / 2, tileSize / 2);
           p5.rotate(angle2);
           p5.rotate(angle);
-          p5.translate(tileSizeWithMargin / -2, tileSizeWithMargin / -2);
+          p5.translate(tileSize / -2, tileSize / -2);
 
           p5.rect(0, 0, tileSize, tileSize);
         });
@@ -213,7 +286,7 @@ export const Grid4: FC<GridProps> = ({
 
       step.current++;
     },
-    [numTilesX, numTilesY, margin],
+    [numTilesX, numTilesY, margin, tileSize, tileSizeWithMargin],
   );
 
   return <ResponsiveSketch setup={setup} draw={draw} />;
