12d3907417e12e5f4412b35e588ba61b04ac4a5f
Kim D. Jeker <git@kije.ch>
2023-09-06 22:11:04 +0200

    day03: build alternative clock

--- src/components/day3/Clock.tsx
+++ src/components/day3/Clock.tsx
@@ -2,13 +2,13 @@ import { ResponsiveSketch } from "@/components/ResponsiveSketch";
 import { PAGE_CONTENT_WIDTH } from "@/utils/consts";
 import { ease } from "@/utils/easings";
 import { forEach, range, map, clamp } from "lodash";
-import { FC, useCallback, useMemo, useRef, useState } from "react";
+import { FC, memo, useCallback, useMemo, useRef, useState } from "react";
 import { SketchProps } from "react-p5";
 import { easing } from "ts-easing";
 
 const CENTER = PAGE_CONTENT_WIDTH / 2;
 
-export const ClockStep1: FC = () => {
+export const ClockStep1: FC = memo(() => {
   const INNER_TICK_NUMBER = 10;
   const INNER_TICK_SIZE = 10;
   const INNER_TICK_MARGIN = 5;
@@ -64,9 +64,9 @@ export const ClockStep1: FC = () => {
   }, []);
 
   return <ResponsiveSketch setup={setup} draw={draw} />;
-};
+});
 
-export const ClockStep2Interesting: FC = () => {
+export const ClockStep2Interesting: FC = memo(() => {
   const INNER_TICK_NUMBER = 10;
   const INNER_TICK_SIZE = 10;
   const INNER_TICK_MARGIN = 5;
@@ -139,9 +139,9 @@ export const ClockStep2Interesting: FC = () => {
   }, []);
 
   return <ResponsiveSketch setup={setup} draw={draw} />;
-};
+});
 
-export const ClockStep2: FC = () => {
+export const ClockStep2: FC = memo(() => {
   const INNER_TICK_NUMBER = 10;
   const INNER_TICK_SIZE = 10;
   const INNER_TICK_MARGIN = 5;
@@ -201,9 +201,9 @@ export const ClockStep2: FC = () => {
   }, []);
 
   return <ResponsiveSketch setup={setup} draw={draw} />;
-};
+});
 
-export const ClockStep3: FC = () => {
+export const ClockStep3: FC = memo(() => {
   const INNER_TICK_NUMBER = 25;
   const INNER_TICK_SIZE = 6;
   const INNER_TICK_MARGIN = 1;
@@ -266,9 +266,9 @@ export const ClockStep3: FC = () => {
   }, []);
 
   return <ResponsiveSketch setup={setup} draw={draw} />;
-};
+});
 
-export const ClockStep4: FC = () => {
+export const ClockStep4: FC = memo(() => {
   const INNER_TICK_NUMBER = 30;
   const INNER_TICK_SIZE = 6;
   const INNER_TICK_MARGIN = 1;
@@ -334,4 +334,455 @@ export const ClockStep4: FC = () => {
   }, []);
 
   return <ResponsiveSketch setup={setup} draw={draw} />;
-};
+});
+
+export const ClockAlternative: FC = memo(() => {
+  // todo DRY this code up ;)
+
+  const MINOR_SECOND_TICK_NUMBER = 6;
+  const MAJOR_SECOND_TICK_NUMBER = 10;
+
+  const MINOR_MINUTES_TICK_NUMBER = 6;
+  const MAJOR_MINUTE_TICK_NUMBER = 10;
+
+  const MINOR_HOURS_TICK_NUMBER = 6;
+  const MAJOR_HOURS_TICK_NUMBER = 4;
+
+  const INNER_TICK_SIZE = 10;
+  const INNER_TICK_MARGIN = 5;
+
+  const MINOR_SECOND_TICK_POSITION = CENTER - 0.6125 * CENTER;
+  const MAJOR_SECOND_TICK_POSITION = CENTER - 0.4375 * CENTER;
+
+  const MINOR_MINUTE_TICK_POSITION = CENTER - 0.0875 * CENTER;
+  const MAJOR_MINUTE_TICK_POSITION = CENTER + 0.0875 * CENTER;
+
+  const MINOR_HOUR_TICK_POSITION = CENTER + 0.4375 * CENTER;
+  const MAJOR_HOUR_TICK_POSITION = CENTER + 0.6125 * CENTER;
+
+  const setup = useCallback<SketchProps["setup"]>((p5, canvasParentRef) => {
+    p5.createCanvas(PAGE_CONTENT_WIDTH, PAGE_CONTENT_WIDTH / 4).parent(
+      canvasParentRef,
+    );
+  }, []);
+
+  const draw = useCallback<NonNullable<SketchProps["draw"]>>((p5) => {
+    p5.clear();
+    p5.stroke(255, 255, 255);
+    p5.strokeWeight(2);
+    //p5.noFill();
+
+    p5.line(
+      MINOR_SECOND_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 10,
+      MINOR_SECOND_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 20,
+    );
+
+    p5.line(
+      MAJOR_SECOND_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 10,
+      MAJOR_SECOND_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 20,
+    );
+
+    p5.stroke(200, 200, 200);
+    p5.strokeWeight(1);
+
+    p5.line(
+      MINOR_SECOND_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 15,
+      MAJOR_SECOND_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 15,
+    );
+
+    p5.stroke(255, 255, 255);
+    p5.strokeWeight(2);
+
+    p5.line(
+      MINOR_MINUTE_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 10,
+      MINOR_MINUTE_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 20,
+    );
+
+    p5.line(
+      MAJOR_MINUTE_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 10,
+      MAJOR_MINUTE_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 20,
+    );
+
+    p5.stroke(200, 200, 200);
+    p5.strokeWeight(1);
+
+    p5.line(
+      MINOR_MINUTE_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 15,
+      MAJOR_MINUTE_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 15,
+    );
+
+    p5.stroke(255, 255, 255);
+    p5.strokeWeight(2);
+
+    p5.line(
+      MINOR_HOUR_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 10,
+      MINOR_HOUR_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 20,
+    );
+
+    p5.line(
+      MAJOR_HOUR_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 10,
+      MAJOR_HOUR_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 20,
+    );
+
+    p5.stroke(200, 200, 200);
+    p5.strokeWeight(1);
+
+    p5.line(
+      MINOR_HOUR_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 15,
+      MAJOR_HOUR_TICK_POSITION,
+      PAGE_CONTENT_WIDTH / 4 - 15,
+    );
+
+    p5.stroke(255, 255, 255);
+    p5.strokeWeight(2);
+
+    p5.strokeWeight(INNER_TICK_SIZE);
+
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const millis = now.getMilliseconds();
+
+    const minorSecondsPassed = seconds % MINOR_SECOND_TICK_NUMBER;
+    const majorSecondsPassed = Math.floor(seconds / MINOR_SECOND_TICK_NUMBER);
+
+    const minorMinutesPassed = minutes % MINOR_MINUTES_TICK_NUMBER;
+    const majorMinutesPassed = Math.floor(minutes / MINOR_MINUTES_TICK_NUMBER);
+
+    const minorHoursPassed = hours % MINOR_HOURS_TICK_NUMBER;
+    const majorHoursPassed = Math.floor(hours / MINOR_HOURS_TICK_NUMBER);
+
+    const minorSecondsReset = minorSecondsPassed === 0 && millis <= 400;
+    const majorSecondsReset = majorSecondsPassed === 0 && minorSecondsReset;
+
+    const minorMinutesReset = minorMinutesPassed === 0 && majorSecondsReset;
+    const majorMinutesReset = majorMinutesPassed === 0 && minorMinutesReset;
+
+    const minorHoursReset = minorHoursPassed === 0 && majorMinutesReset;
+    const majorHoursReset = majorHoursPassed === 0 && minorHoursReset;
+
+    // minor seconds
+    forEach(range(0, MINOR_SECOND_TICK_NUMBER), (i) => {
+      const offsetIndex = MINOR_SECOND_TICK_NUMBER / 2 - i;
+      let offsetX = 0;
+      let offsetY = (INNER_TICK_SIZE + INNER_TICK_MARGIN) * offsetIndex;
+
+      if (minorSecondsReset) {
+        offsetX += ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+
+        offsetY -= ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+      } else if (minorSecondsPassed >= i) {
+        if (minorSecondsPassed === i) {
+          offsetX += ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+          offsetY -= ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+        } else {
+          offsetX += INNER_TICK_SIZE + INNER_TICK_MARGIN;
+          offsetY -= INNER_TICK_SIZE + INNER_TICK_MARGIN;
+        }
+      }
+
+      p5.point(MINOR_SECOND_TICK_POSITION + offsetX, CENTER / 4 + offsetY);
+    });
+
+    // major seconds
+    forEach(range(0, MAJOR_SECOND_TICK_NUMBER), (i) => {
+      const offsetIndex = MAJOR_SECOND_TICK_NUMBER / 2 - i;
+      let offsetX = 0;
+      let offsetY = (INNER_TICK_SIZE + INNER_TICK_MARGIN) * offsetIndex;
+
+      if (majorSecondsReset) {
+        offsetX += ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+
+        offsetY -= ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+      } else if (majorSecondsPassed >= i) {
+        if (majorSecondsPassed === i && minorSecondsPassed === 0) {
+          offsetX += ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+          offsetY -= ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+        } else {
+          offsetX += INNER_TICK_SIZE + INNER_TICK_MARGIN;
+          offsetY -= INNER_TICK_SIZE + INNER_TICK_MARGIN;
+        }
+      }
+
+      p5.point(MAJOR_SECOND_TICK_POSITION + offsetX, CENTER / 4 + offsetY);
+    });
+
+    // minor minutes
+    forEach(range(0, MINOR_MINUTES_TICK_NUMBER), (i) => {
+      const offsetIndex = MINOR_MINUTES_TICK_NUMBER / 2 - i;
+      let offsetX = 0;
+      let offsetY = (INNER_TICK_SIZE + INNER_TICK_MARGIN) * offsetIndex;
+
+      if (minorMinutesReset) {
+        offsetX += ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+
+        offsetY -= ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+      } else if (minorMinutesPassed >= i) {
+        if (
+          minorMinutesPassed === i &&
+          majorSecondsPassed === 0 &&
+          minorSecondsPassed === 0
+        ) {
+          offsetX += ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+          offsetY -= ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+        } else {
+          offsetX += INNER_TICK_SIZE + INNER_TICK_MARGIN;
+          offsetY -= INNER_TICK_SIZE + INNER_TICK_MARGIN;
+        }
+      }
+
+      p5.point(MINOR_MINUTE_TICK_POSITION + offsetX, CENTER / 4 + offsetY);
+    });
+
+    // major minutes
+    forEach(range(0, MAJOR_MINUTE_TICK_NUMBER), (i) => {
+      const offsetIndex = MAJOR_MINUTE_TICK_NUMBER / 2 - i;
+      let offsetX = 0;
+      let offsetY = (INNER_TICK_SIZE + INNER_TICK_MARGIN) * offsetIndex;
+
+      if (majorMinutesReset) {
+        offsetX += ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+
+        offsetY -= ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+      } else if (majorMinutesPassed >= i) {
+        if (
+          majorMinutesPassed === i &&
+          minorMinutesPassed === 0 &&
+          majorSecondsPassed === 0 &&
+          minorSecondsPassed === 0
+        ) {
+          offsetX += ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+          offsetY -= ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+        } else {
+          offsetX += INNER_TICK_SIZE + INNER_TICK_MARGIN;
+          offsetY -= INNER_TICK_SIZE + INNER_TICK_MARGIN;
+        }
+      }
+
+      p5.point(MAJOR_MINUTE_TICK_POSITION + offsetX, CENTER / 4 + offsetY);
+    });
+
+    // minor hours
+    forEach(range(0, MINOR_HOURS_TICK_NUMBER), (i) => {
+      const offsetIndex = MINOR_HOURS_TICK_NUMBER / 2 - i;
+      let offsetX = 0;
+      let offsetY = (INNER_TICK_SIZE + INNER_TICK_MARGIN) * offsetIndex;
+
+      if (minorHoursReset) {
+        offsetX += ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+
+        offsetY -= ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+      } else if (minorHoursPassed >= i) {
+        if (
+          minorHoursPassed === i &&
+          majorMinutesPassed === 0 &&
+          minorMinutesPassed === 0 &&
+          majorSecondsPassed === 0 &&
+          minorSecondsPassed === 0
+        ) {
+          offsetX += ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+          offsetY -= ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+        } else {
+          offsetX += INNER_TICK_SIZE + INNER_TICK_MARGIN;
+          offsetY -= INNER_TICK_SIZE + INNER_TICK_MARGIN;
+        }
+      }
+
+      p5.point(MINOR_HOUR_TICK_POSITION + offsetX, CENTER / 4 + offsetY);
+    });
+
+    // major hours
+    forEach(range(0, MAJOR_HOURS_TICK_NUMBER), (i) => {
+      const offsetIndex = MAJOR_HOURS_TICK_NUMBER / 2 - i;
+      let offsetX = 0;
+      let offsetY = (INNER_TICK_SIZE + INNER_TICK_MARGIN) * offsetIndex;
+
+      if (majorHoursReset) {
+        offsetX += ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+
+        offsetY -= ease(
+          INNER_TICK_SIZE + INNER_TICK_MARGIN,
+          0,
+          millis / 400,
+          easing.outCubic,
+          p5,
+        );
+      } else if (majorHoursPassed >= i) {
+        if (
+          majorHoursPassed === i &&
+          minorHoursPassed === 0 &&
+          majorMinutesPassed === 0 &&
+          minorMinutesPassed === 0 &&
+          majorSecondsPassed === 0 &&
+          minorSecondsPassed === 0
+        ) {
+          offsetX += ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+          offsetY -= ease(
+            0,
+            INNER_TICK_SIZE + INNER_TICK_MARGIN,
+            millis / 1000,
+            easing.inOutQuart,
+            p5,
+          );
+        } else {
+          offsetX += INNER_TICK_SIZE + INNER_TICK_MARGIN;
+          offsetY -= INNER_TICK_SIZE + INNER_TICK_MARGIN;
+        }
+      }
+
+      p5.point(MAJOR_HOUR_TICK_POSITION + offsetX, CENTER / 4 + offsetY);
+    });
+  }, []);
+
+  return <ResponsiveSketch setup={setup} draw={draw} />;
+});
